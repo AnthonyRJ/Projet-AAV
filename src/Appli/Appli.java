c7e1264b045fc020f7da3e9c2b60d32695ab34a1
Anthony RJ <anthonyjoaquim40@gmail.com>
2019-10-01 23:52:50 +0200

    Debut de la méthode dynamique

--- src/Appli/Appli.java
+++ src/Appli/Appli.java
@@ -1,4 +1,7 @@
 package Appli;
+import java.util.ArrayList;
+
+import MethodeResolution.Dynamique;
 import MethodeResolution.Gloutonne;
 import Outils.Objet;
 import Outils.SacADos;
@@ -6,7 +9,8 @@ import Outils.SacADos;
 public class Appli {
 
 	public static void main(String[] args) {
-		Objet lampe = new Objet("lampe",2.0,30.0);
+		/* TEST METHODE GLOUTONNE*/
+		/*Objet lampe = new Objet("lampe",2.0,30.0);
 		Objet SacDeCouchage = new Objet("SacDeCouchage",1.0,20.0);
 		Objet CampingGaz = new Objet("CampingGaz", 3.0, 40.0);
 		Objet CouteauSuisse = new Objet("CouteauSuisse",0.2,50.0);
@@ -23,7 +27,34 @@ public class Appli {
 		Gloutonne methodeGlou = new Gloutonne(liste,sac);
 		
 		methodeGlou.resoudre();
-		System.out.println(methodeGlou.toString());
+		System.out.println(methodeGlou.toString());*/
+		
+		/*TEST METHODE DYNAMIQUE*/
+		Objet lampe = new Objet("lampe",2.0,5.0);
+		Objet SacDeCouchage = new Objet("SacDeCouchage",3.0,8.0);
+		Objet CampingGaz = new Objet("CampingGaz", 5.0, 14.0);
+		Objet CouteauSuisse = new Objet("CouteauSuisse",2.0,6.0);
+		Objet Snickers = new Objet("Snickers",4.0,13.0);
+		Objet Tente = new Objet("Tente",6.0,17.0);
+		Objet Briquet = new Objet("Briquet",3.0,10.0);
+		Objet Coca = new Objet("Coca", 1.0,4.0);
+		
+		SacADos sac = new SacADos(12.0);
+		
+		ArrayList<Objet> liste = new ArrayList<>();
+		
+		liste.add(lampe);
+		liste.add(SacDeCouchage);
+		liste.add(CampingGaz);
+		liste.add(CouteauSuisse);
+		liste.add(Snickers);
+		liste.add(Tente);
+		liste.add(Briquet);
+		liste.add(Coca);
+		
+		Dynamique methodeDyna = new Dynamique(sac,liste);
+		
+		methodeDyna.remplissageMatrice();
 		
 	}
 
--- src/MethodeResolution/Dynamique.java
+++ src/MethodeResolution/Dynamique.java
@@ -1,5 +1,59 @@
-package MethodeResolution;
+ package MethodeResolution;
+
+import java.util.ArrayList;
+
+import Outils.Objet;
+import Outils.SacADos;
 
 public class Dynamique {
-
+	private SacADos sac;
+	private ArrayList<Objet> objets;
+	private int[][] matrice;
+	
+	public Dynamique(SacADos s, ArrayList<Objet> l) {
+		this.sac = s;
+		this.objets = l;
+		this.matrice = new int[l.size()][(int) (s.getCapacite() + 1)];
+	}
+	
+	/*probleme si la capacite du sac est supérieur au nombre d'objet*/
+	public void remplissageMatrice() {
+		for(int j = 0; j <  this.sac.getCapacite(); j++) {
+			if (this.objets.get(j).getPoids() > j) {
+				this.matrice[0][j] = 0;
+			}
+			else {
+				this.matrice[0][j] = (int) this.objets.get(j).getVal();
+			}
+			
+			for (int i = 1; i < this.sac.getCapacite() + 1; i++) {
+				if (this.objets.get(i).getPoids() > j) {
+					this.matrice[i][j] = this.matrice[i-1][j];
+				}
+				else {
+					this.matrice[i][j] = max(this.matrice[i-1][j], 
+					this.matrice[i-1][(int) (j-this.objets.get(i).getPoids() + 
+					this.objets.get(i).getVal())]);
+				}
+			}
+			
+		}
+		
+		
+	}
+	
+	/*
+	 * retourne la plus grande valeur entre val1 et val2
+	 */
+	private int max(int val1, int val2) {
+		if (val1 > val2) {
+			return val1;
+		}
+		else {
+			return val2;
+		}
+	}
+	
+	
+	
 }
